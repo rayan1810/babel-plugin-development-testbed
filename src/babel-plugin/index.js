6b33307fd005e59a43502d9bf22c342c433da907
Rohit Singh <rohit99.ind@gmail.com>
2024-03-09 15:43:25 +0530

    feat: init

---
+++ src/babel-plugin/index.js
@@ -0,0 +1,15 @@
+module.exports = function ({ types: t }) {
+  return {
+    visitor: {
+      CallExpression(path) {
+        if (
+          t.isMemberExpression(path.node.callee) &&
+          t.isIdentifier(path.node.callee.object, { name: "console" }) &&
+          t.isIdentifier(path.node.callee.property, { name: "log" })
+        ) {
+          path.remove();
+        }
+      },
+    },
+  };
+};
--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,13 +1,23 @@
+// @ts-nocheck
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
-
+import babelPlugin from "../../babel-plugin";
+import { transformSync } from "@babel/core";
+import generator from "@babel/generator";
 type Data = {
   name: string;
 };
 
 export default function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>,
+  res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: "John Doe" });
+  const transformedCode = transformSync(req.body, {
+    // add support for JSX
+    presets: ["@babel/preset-react"],
+    plugins: [babelPlugin],
+    ast: true,
+  });
+  const result = generator(transformedCode.ast, { sourceType: "module" });
+  res.status(200).json({ code: result });
 }
